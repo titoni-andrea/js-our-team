855b9801026d20afe7d7d40a99a262cb00da5ec6
titoni-andrea <andreatitoni@gmail.com>
2024-05-06 16:39:53 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,33 @@
+let team = [
+    {
+        nome: "Wayne Barnett",
+        ruolo: "Founder & CEO",
+        immagine: "wayne-barnett-founder-ceo.jpg",
+    },
+    {
+        nome: "Angela Caroll",
+        ruolo: "Chief Editor",
+        immagine: "angela-caroll-chief-editor.jpg",
+    },
+    {
+        nome: "Walter Gordon",
+        ruolo: "Office Manager",
+        immagine: "walter-gordon-office-manager.jpg",
+    },
+    {
+        nome: "Angela Lopez",
+        ruolo: "Social Media Manager",
+        immagine: "angela-lopez-social-media-manager.jpg",
+    },
+    {
+        nome: "Scott Estrada",
+        ruolo: "Developer",
+        immagine: "scott-estrada-developer.jpg",
+    },
+    {
+        nome: "Barbara Ramos",
+        ruolo: "Graphic Designer",
+        immagine: "barbara-ramos-graphic-designer.jpg",
+    },
+    
+]
